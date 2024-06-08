dce3f8332040d75982b1f917f5dc3aff2c95ea91
Vitor Martins <vitormartins281261@gmail.com>
2024-06-08 16:40:27 -0300

    Feat: adcionado tela de login

--- src/App.jsx
+++ src/App.jsx
@@ -14,16 +14,11 @@ function App() {
   const [price, setPrice] = useState("");
   const [stock, setStock] = useState("");
   const [edit, setEdit] = useState(false);
-  const [login, setLogin] = useState ("")
-  const [userLogin, setUserLogin] = useState ("")
-  const [password, setPassword] = useState ("")
-  const [userPassword, setUserPassword] = useState ("")
-  const [error, setError] = useState (null)
+
 
 
   const url = 'http://localhost:3000/products';
-  const userUrl = 'http://localhost:3000/users'
-
+  
   useEffect(() => {
     // Lista todos os produtos:
     const getProductsList = async() => {
@@ -57,36 +52,6 @@ function App() {
     setEdit(true);
   }
 
-  const getUserByLogin = async(login) => {
-    try{
-    // Faz a requisição http
-    const res = await fetch(userUrl);
-    const data = await res.json();
-    // Carrega os dados no formulário para edição:
-console.log('usuarios',data);
-      console.log('login',login);
-    const userLogin = data.find((user) => {
-      console.log('LOGIN DOS USUARIOS',user.login)
-      console.log('teste ',String(user.login).toLowerCase() == login.toLowerCase() );
-      return user.login == login
-    })
-
-console.log('achou usuario', userLogin);
-
-      if(userLogin === undefined) {
-        setError("Usuario não encontrado")
-        return
-      }
-
-    setUserLogin(userLogin.login)
-    setUserPassword(userLogin.password);
-    setError(null)
-    }catch(err){
-      console.log('DEU ERRO', err);
-      setError("Falha ao realizar o login")
-    }
-
-  }
 
   const saveProduct = async (e) => {
     e.preventDefault();
@@ -143,24 +108,10 @@ console.log('achou usuario', userLogin);
   const handlePrice = (e) => {setPrice(e.target.value)};
   const handleStock = (e) => {setStaock(e.target.value)};
 
-  const handleLogin = (e) => {setLogin(e.target.value)};
-  const handlePassword = (e) => {setPassword(e.target.value)};
-
-  const loginUser = async (e) => {
-    e.preventDefault();
-
-await getUserByLogin(login)
-  
-    console.log(userLogin);
-    console.log(userPassword)
-
-    console.log(e)
-  }
-
 
   return (
     <>
-     <h2>CRUD com JSON Server</h2>
+     <h2>Listagem Produtos</h2>
      <div>
         {
           products.length > 0 ? <ProductTable products={products} deleteProduct={deleteProduct} editProduct={getProductById} /> : <h3 style={{marginBottom: '30px'}}>Nenhum produto cadastrado...</h3>
@@ -168,7 +119,6 @@ await getUserByLogin(login)
       </div>
 
       <ProductForm name={name} price={price} stock={stock} handleName={handleName} handlePrice={handlePrice} handleStock={handleStock} saveProduct={saveProduct}/>
-      <LoginForm login={login} password={password} handleLogin={handleLogin} handlePassword={handlePassword} loginUser={loginUser} error={error}/>
     </>
   )
 }
